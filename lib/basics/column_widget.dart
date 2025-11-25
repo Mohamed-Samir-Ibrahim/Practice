3e77f2dcee600cfc143c97acb8e8ec0b8add9488
Mohamed Samir <mohamed.samir.ibrahim98@gmail.com>
2025-11-25 09:46:42 +0200

    fix: added mainAxisAlignment, crossAxisAlignment and mainAxisSize and .max in mainAxisSize.

--- lib/basics/column_widget.dart
+++ lib/basics/column_widget.dart
@@ -11,12 +11,25 @@ class _ColumnWidgetState extends State<ColumnWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black38,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Flutter'),
       ),
       body: Center(
-        child: Column(mainAxisAlignment: .center, children: []),
+        child: Container(
+          color: Colors.green,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .start,
+            mainAxisSize: .max,
+            children: [
+              Container(color: Colors.red, width: 100, height: 100),
+              Container(color: Colors.blue, width: 100, height: 100),
+            ],
+          ),
+        ),
       ),
     );
   }
